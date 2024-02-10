b6f61e2b75c0f048ec265555096aef3321b65b48
nj <nickgentrymma@gmail.com>
2024-02-10 02:17:53 -0800

    added pictures and effects.  also added favicon

--- frontend/src/Components/top/top.jsx
+++ frontend/src/Components/top/top.jsx
@@ -1,6 +1,19 @@
+import React, { useEffect } from 'react';
 import './top.css';
 
 const Top = () => {
+    
+    useEffect(() => {
+        const mainPicPair = document.getElementById('main-pic-pair');
+        const mainPicLeft = mainPicPair.querySelector('.main-pic-left');
+        const mainPicRight = mainPicPair.querySelector('.main-pic-right');
+
+        setTimeout(() => {
+            mainPicLeft.classList.add('slide-in');
+            mainPicRight.classList.add('slide-in');
+        }, 800);
+    }, []);
+
 return (
     <div className="body" >
         <div className="top-left">
@@ -11,8 +24,8 @@ return (
             </span>
         </div>
         <div className="picture-pair" id="main-pic-pair">
-            <img className="main-pic-left" data-aos="slide-in" src="images/iCloud Photos from Troy beecher/top.JPEG" alt=""/>
-            <img className="main-pic-right" data-aos="slide-in" src="images/iCloud Photos from Troy beecher/top2.JPEG" alt=""/>
+            <img className="main-pic-left" data-aos="slide-in" src={require("../../Assets/Images/Rims/top.JPEG")} alt=""/>
+            <img className="main-pic-right" data-aos="slide-in" src={require("../../Assets/Images/Rims/top2.JPEG")} alt=""/>
         </div>
         {/* <!-- will replace these before and after pics when correct video is available. -->
         <!-- <iframe width="100%" height="315" src="UNCLE T VID" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> --> */}
