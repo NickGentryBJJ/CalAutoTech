bee8cc143cd151fc9e89272d1b922d90dfa7298a
nj <nickgentrymma@gmail.com>
2024-02-14 19:06:54 -0800

    Added Favicongit add -A

--- frontend/src/Components/Services/Services.jsx
+++ frontend/src/Components/Services/Services.jsx
@@ -1,41 +1,38 @@
 import './Services.css';
 
 const Services = () => {
-    
-    
-        document.addEventListener('DOMContentLoaded', function () {
-                const leftImages = document.querySelectorAll('.b-a-pic-left');
-                const rightImages = document.querySelectorAll('.b-a-pic-right');
-                const servicesText = document.querySelectorAll('.services-text');
-                
-                const observer = new IntersectionObserver(entries => {
-                    entries.forEach(entry => {
-                        if (entry.isIntersecting) {
-                            entry.target.classList.add('slide-in');
-                            observer.unobserve(entry.target);
-                        }
-                    });
-                }, { threshold: 0.2 }); // Adjust the threshold as needed
-                const observert = new IntersectionObserver(entries => {
-                    entries.forEach(entry => {
-                        if (entry.isIntersecting) {
-                            entry.target.classList.add('slide-inn');
-                            observer.unobserve(entry.target);
-                        }
-                    });
-                }, { threshold: 0.002 }); // Adjust the threshold as needed
-            
-                leftImages.forEach(image => {
-                    observer.observe(image);
-                });
-                rightImages.forEach(image => {
-                    observer.observe(image);
-                });
-                servicesText.forEach(image => {
-                    observert.observe(image);
-                });
+    document.addEventListener('DOMContentLoaded', function () {
+        const leftImages = document.querySelectorAll('.b-a-pic-left');
+        const rightImages = document.querySelectorAll('.b-a-pic-right');
+        const servicesText = document.querySelectorAll('.services-text');
+        
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('slide-in');
+                    observer.unobserve(entry.target);
+                }
             });
-
+        }, { threshold: 0.2 }); // Adjust the threshold as needed
+        const observert = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('slide-inn');
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, { threshold: 0.002 }); // Adjust the threshold as needed
+        
+        leftImages.forEach(image => {
+            observer.observe(image);
+        });
+        rightImages.forEach(image => {
+            observer.observe(image);
+        });
+        servicesText.forEach(image => {
+            observert.observe(image);
+        });
+    });
     return (
         // <!-- SERVICES SECTION -->
         <div id="services-section" className="services-sec">
@@ -57,28 +54,68 @@ const Services = () => {
                     Discounts for multiple wheels, as well as special rates for military and seniors.
                 </p>
             </div>
-
+            {/* GALLERY SECTION */}
             <div className="before-after-pics">
                 <div className="left-b-a">
                     <div className="picture-pair">
-                        <img className="b-a-pic-left" data-aos="slide-in" src={require("../../Assets/Images/Rims/2ndtop.JPEG")} alt=""/>
-                        <img className="b-a-pic-right" data-aos="slide-in" src={require("../../Assets/Images/Rims/2ndbottom.JPEG")} alt=""/>
+                        <img 
+                            className="b-a-pic-left" 
+                            data-aos="slide-in" 
+                            src={require("../../Assets/Images/Rims/2ndtop.JPEG")} 
+                            alt="Before" />
+                        <img 
+                            className="b-a-pic-right"
+                            data-aos="slide-in" 
+                            src={require("../../Assets/Images/Rims/2ndbottom.JPEG")} 
+                            alt="After" />
                     </div>
                     <div className="picture-pair">
-                        <img className="b-a-pic-left" data-aos="slide-in" src={require("../../Assets/Images/Rims/3rdtop.JPEG")} alt="" />
-                        <img className="b-a-pic-right" data-aos="slide-in" src={require("../../Assets/Images/Rims/3rdbottom.JPEG")} alt="" />
+                        <img 
+                            className="b-a-pic-left" 
+                            data-aos="slide-in" 
+                            src={require("../../Assets/Images/Rims/3rdtop.JPEG")} 
+                            alt="Before" />
+                        <img 
+                            className="b-a-pic-right" 
+                            data-aos="slide-in" 
+                            src={require("../../Assets/Images/Rims/3rdbottom.JPEG")} 
+                            alt="After" />
                     </div>
                     <div className="picture-pair">
-                        <img className="b-a-pic-left" data-aos="slide-in" src={require("../../Assets/Images/Rims/4thtop.JPEG")} alt="" />
-                        <img className="b-a-pic-right" data-aos="slide-in" src={require("../../Assets/Images/Rims/4thbottom.JPEG")} alt="" />
+                        <img 
+                            className="b-a-pic-left" 
+                            data-aos="slide-in" 
+                            src={require("../../Assets/Images/Rims/4thtop.JPEG")} 
+                            alt="Before" />
+                        <img 
+                            className="b-a-pic-right" 
+                            data-aos="slide-in" 
+                            src={require("../../Assets/Images/Rims/4thbottom.JPEG")} 
+                            alt="After" />
                     </div>
                     <div className="picture-pair">
-                        <img className="b-a-pic-left" data-aos="slide-in" src={require("../../Assets/Images/Rims/5thtop.JPEG")} alt="" />
-                        <img className="b-a-pic-right" data-aos="slide-in" src={require("../../Assets/Images/Rims/5thbottom.PNG")} alt="" />
+                        <img 
+                            className="b-a-pic-left" 
+                            data-aos="slide-in" 
+                            src={require("../../Assets/Images/Rims/5thtop.JPEG")} 
+                            alt="Before" />
+                        <img 
+                            className="b-a-pic-right" 
+                            data-aos="slide-in" 
+                            src={require("../../Assets/Images/Rims/5thbottom.PNG")} 
+                            alt="After" />
                     </div>
                     <div className="picture-pair">
-                        <img className="b-a-pic-left" data-aos="slide-in" src={require("../../Assets/Images/Rims/lasttop.JPEG")} alt="" />
-                        <img className="b-a-pic-right" data-aos="slide-in" src={require("../../Assets/Images/Rims/lastbottom.JPEG")} alt="" />
+                        <img 
+                            className="b-a-pic-left"   
+                            data-aos="slide-in" 
+                            src={require("../../Assets/Images/Rims/lasttop.JPEG")} 
+                            alt="Before" />
+                        <img 
+                            className="b-a-pic-right" 
+                            data-aos="slide-in" 
+                            src={require("../../Assets/Images/Rims/lastbottom.JPEG")} 
+                            alt="After" />
                     </div>
                 </div>
             </div>
